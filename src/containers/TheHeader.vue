97b5803d11abcdf5d0068399846fbb467bea570c
60160113 <60160113@go.buu.ac.th>
2021-10-03 21:28:50 +0700

    Update TheHeader.vue

--- src/containers/TheHeader.vue
+++ src/containers/TheHeader.vue
@@ -11,68 +11,37 @@
       @click="$store.commit('toggleSidebarDesktop')"
     />
     <CHeaderBrand class="mx-auto d-lg-none" to="/">
-      <CIcon name="logo" height="48" alt="Logo"/>
+      <CIcon name="logo" height="48" alt="Logo" />
     </CHeaderBrand>
-    <CHeaderNav class="d-md-down-none mr-auto">
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink to="/dashboard">
-          Dashboard
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink to="/users" exact>
-          Users
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-      <CHeaderNavItem class="px-3">
-        <CHeaderNavLink>
-          Settings
-        </CHeaderNavLink>
-      </CHeaderNavItem>
-    </CHeaderNav>
+    <CHeaderNav class="d-md-down-none mr-auto"> </CHeaderNav>
     <CHeaderNav>
       <CHeaderNavItem class="px-3 c-d-legacy-none">
         <button
           @click="() => $store.commit('toggle', 'darkMode')"
           class="c-header-nav-btn"
         >
-          <CIcon v-if="$store.state.darkMode" name="cil-sun"/>
-          <CIcon v-else name="cil-moon"/>
+          <CIcon v-if="$store.state.darkMode" name="cil-sun" />
+          <CIcon v-else name="cil-moon" />
         </button>
       </CHeaderNavItem>
-      <TheHeaderDropdownNotif/>
-      <TheHeaderDropdownTasks/>
-      <TheHeaderDropdownMssgs/>
-      <TheHeaderDropdownAccnt/>
-      <CHeaderNavItem class="px-3">
-        <button
-          class="c-header-nav-btn"
-          @click="$store.commit('toggle', 'asideShow')"
-        >
-          <CIcon size="lg" name="cil-applications-settings" class="mr-2"/>
-        </button>
-      </CHeaderNavItem>
+      <TheHeaderDropdownAccnt />
     </CHeaderNav>
-
-    <CSubheader class="px-3">
-      <CBreadcrumbRouter class="border-0 mb-0"/>
-    </CSubheader>
   </CHeader>
 </template>
 
 <script>
-import TheHeaderDropdownAccnt from './TheHeaderDropdownAccnt'
-import TheHeaderDropdownNotif from './TheHeaderDropdownNotif'
-import TheHeaderDropdownTasks from './TheHeaderDropdownTasks'
-import TheHeaderDropdownMssgs from './TheHeaderDropdownMssgs'
+import TheHeaderDropdownAccnt from "./TheHeaderDropdownAccnt";
+import TheHeaderDropdownNotif from "./TheHeaderDropdownNotif";
+import TheHeaderDropdownTasks from "./TheHeaderDropdownTasks";
+import TheHeaderDropdownMssgs from "./TheHeaderDropdownMssgs";
 
 export default {
-  name: 'TheHeader',
+  name: "TheHeader",
   components: {
     TheHeaderDropdownAccnt,
     TheHeaderDropdownNotif,
     TheHeaderDropdownTasks,
-    TheHeaderDropdownMssgs
-  }
-}
+    TheHeaderDropdownMssgs,
+  },
+};
 </script>
