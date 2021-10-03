67cbbb1fefc0de122848aca9f751abc0b1d1b20c
phukeiei <34035927+phukeiei@users.noreply.github.com>
2021-10-04 01:46:50 +0700

    add menu

---
+++ src/views/change_request/changeRequest.vue
@@ -0,0 +1,452 @@
+<template>
+  <div>
+    <CCard>
+      <CCardHeader>
+        <h4>
+          ตารางรายการจัดการการเปลี่ยนแปลง
+        </h4>
+      </CCardHeader>
+      <CCardBody>
+        <CDataTable
+          :items="changeRequestList"
+          :fields="changeRequestFields"
+          :loading="tableLoading"
+          column-filter
+          table-filter
+          items-per-page-select
+          :items-per-page="10"
+          hover
+          sorter
+          pagination
+        >
+          <template #no-items-view>
+            <div class="d-flex justify-content-center">
+              ไม่พบรายการจัดการการเปลี่ยนแปลง
+            </div>
+          </template>
+          <template #over-table>
+            <CButton
+              class="mb-3"
+              size="sm"
+              color="success"
+              @click="changeRequestCreateModal = true"
+              >ขออนุมัติดำเนินการเปลี่ยนแปลง</CButton
+            >
+          </template>
+          <template #index="{ index }">
+            <td>
+              {{ index + 1 }}
+            </td>
+          </template>
+          <template #processName="{item}">
+            <td>
+              <CBadge :color="getBadge(item.processName)">
+                {{ item.processName }}
+              </CBadge>
+            </td>
+          </template>
+          <template #action="{ item }">
+            <td>
+              <CDropdown
+                color="info"
+                size="sm"
+                toggler-text=""
+                class="ml-1"
+                placement="left-start"
+                v-if="item.processName == 'รออนุมัติ'"
+              >
+                <div>
+                  <div>
+                    <CDropdownItem
+                      @click="considerRequestPage(item.finAdvanceId)"
+                      >ตรวจสอบคำร้อง</CDropdownItem
+                    >
+                    <CDropdownDivider></CDropdownDivider>
+                  </div>
+                  <CDropdownItem @click="openchangeRequestEdit(item)"
+                    >แก้ไขคำร้อง</CDropdownItem
+                  >
+                  <CDropdownItem @click="checkDelete(item)"
+                    >ลบคำร้อง</CDropdownItem
+                  >
+                </div></CDropdown
+              >
+              <CDropdown
+                color="info"
+                size="sm"
+                toggler-text=""
+                class="ml-1"
+                placement="left-start"
+                v-if="item.processName == 'รอตรวจสอบ'"
+              >
+                <div>
+                  <div>
+                    <CDropdownItem
+                      @click="considerRequestPage(item.finAdvanceId)"
+                      >ตรวจสอบคำร้อง</CDropdownItem
+                    >
+                    <CDropdownDivider></CDropdownDivider>
+                  </div>
+                  <CDropdownItem @click="openchangeRequestEdit(item)"
+                    >แก้ไขคำร้อง</CDropdownItem
+                  >
+                  <CDropdownItem @click="checkDelete(item)"
+                    >ลบคำร้อง</CDropdownItem
+                  >
+                </div></CDropdown
+              >
+              <CDropdown
+                color="info"
+                size="sm"
+                toggler-text=""
+                class="ml-1"
+                placement="left-start"
+                v-if="item.processName == 'อนุมัติ'"
+              >
+                <div>
+                  <div>
+                    <CDropdownItem
+                      @click="considerRequestPage(item.finAdvanceId)"
+                      >ตรวจสอบผลดำเนินการ</CDropdownItem
+                    >
+                  </div>
+                </div></CDropdown
+              >
+            </td>
+          </template>
+        </CDataTable>
+      </CCardBody>
+    </CCard>
+    <CModal
+      title="ขออนุมัติดำเนินการเปลี่ยนแปลง (Change Request Form)"
+      color="info"
+      :show.sync="changeRequestCreateModal"
+      centered
+    >
+      <CTextarea
+        label="มีความประสงค์ขอเปลี่ยนแปลงเรื่อง"
+        horizontal
+        v-model="newChangeRequest.subject"
+      />
+      <CSelect
+        label="เหตุผลที่ขอเปลี่ยนแปลง"
+        placeholder="กรุณาเลือกเหตุผลที่ขอเปลี่ยนแปลง"
+        :value.sync="newChangeRequest.reason"
+        horizontal
+        :options="reasonList"
+      />
+      <template #footer>
+        <CButton color="secondary" @click="changeRequestCreateModal = false"
+          >ยกเลิก</CButton
+        >
+        <CButton color="success" @click="changeRequestCreate()">ยืนยัน</CButton>
+      </template>
+    </CModal>
+
+    <CModal
+      title="แก้ไขขออนุมัติดำเนินการเปลี่ยนแปลง (Change Request Form)"
+      color="warning"
+      :show.sync="changeRequestEditModal"
+      centered
+    >
+      <CTextarea
+        label="มีความประสงค์ขอเปลี่ยนแปลงเรื่อง"
+        horizontal
+        v-model="editChangeRequest.subject"
+      />
+      <CSelect
+        label="เหตุผลที่ขอเปลี่ยนแปลง"
+        placeholder="กรุณาเลือกเหตุผลที่ขอเปลี่ยนแปลง"
+        :value.sync="editChangeRequest.reason"
+        horizontal
+        :options="reasonList"
+      />
+      <template #footer>
+        <CButton color="secondary" @click="changeRequestEditModal = false"
+          >ยกเลิก</CButton
+        >
+        <CButton color="success" @click="changeRequestEdit()">ยืนยัน</CButton>
+      </template>
+    </CModal>
+  </div>
+</template>
+<script>
+import { jogetService } from "@/helpers/joget-helper";
+
+export default {
+  components: {
+    jogetService
+  },
+  data() {
+    return {
+      infoAuth: [],
+      tableLoading: false,
+      changeRequestCreateModal: false,
+      changeRequestEditModal: false,
+      // changeRequestList: [],
+      changeRequestFields: [
+        { key: "index", label: "#", _style: "width:5%" },
+        {
+          key: "changeRequestId",
+          label: "เลขที่เอกสาร",
+          _style: "min-width:5%"
+        },
+        {
+          key: "dateCreated",
+          label: "วันที่ร้องขอ",
+          _style: "min-width:10%"
+        },
+        {
+          key: "subject",
+          label: "ความประสงค์",
+          _style: "min-width:10%"
+        },
+        {
+          key: "reason",
+          label: "เหตุผล",
+          _style: "min-width:10%"
+        },
+        {
+          key: "requester",
+          label: "ผู้ร้องขอ",
+          _style: "min-width:10%"
+        },
+        { key: "processName", label: "สถานะ", _style: "min-width:5%" },
+        { key: "action", label: "", _style: "width:5%;" }
+      ],
+      projectList: [],
+      reasonList: [
+        { value: "new", label: "เพิ่มระบบใหม่" },
+        { value: "edit", label: "ปรับปรุง/แก้ไข" },
+        { value: "other", label: "อื่นๆ" }
+      ],
+      departmentList: [],
+      newChangeRequest: {
+        subject: "",
+        reason: "",
+        createBy: "",
+        modiflyBy: "",
+        processId: ""
+      },
+      editChangeRequest: {
+        id: "",
+        subject: "",
+        reason: "",
+        modiflyBy: "",
+        processId: ""
+      },
+      projectIndex: 0,
+      departmentIndex: 0,
+      department: "",
+      id: "",
+      changeRequestList: [
+        {
+          changeRequestId: "F-IT-CO-06.01-01/01",
+          requester: "Samppa Nori",
+          dateCreated: "2012/01/01",
+          subject: "เพิ่มระบบ A101",
+          reason: "เพิ่มระบบใหม่",
+          processName: "รออนุมัติ"
+        },
+        {
+          changeRequestId: "F-IT-CO-06.01-01/02",
+          requester: "Estavan Lykos",
+          dateCreated: "2012/02/01",
+          subject: "แก้ไข A109",
+          reason: "ปรับปรุง/แก้ไข",
+          processName: "รอตรวจสอบ"
+        },
+        {
+          changeRequestId: "F-IT-CO-06.01-01/03",
+          requester: "Chetan Mohamed",
+          dateCreated: "2012/02/01",
+          subject: "ตรวจสอบความถูกต้องของ A500",
+          reason: "อื่นๆ",
+          processName: "รออนุมัติ"
+        },
+        {
+          changeRequestId: "F-IT-CO-06.01-01/04",
+          requester: "Derick Maximinus",
+          dateCreated: "2012/03/01",
+          reason: "เพิ่มระบบใหม่",
+          subject: "เพิ่มระบบ A103",
+          processName: "เสร็จสิ้น"
+        },
+        {
+          changeRequestId: "F-IT-CO-06.01-01/05",
+          requester: "Friderik Dávid",
+          dateCreated: "2012/01/21",
+          subject: "ปรับปรุง/แก้ไข B101",
+          reason: "ปรับปรุง/แก้ไข",
+          processName: "รอผลดำเนินการ"
+        }
+      ]
+    };
+  },
+  created() {
+    // this.tableLoading = true;
+    this.infoAuth = JSON.parse(localStorage.getItem("AuthUser"));
+    this.newChangeRequest.createBy = this.infoAuth.fullname;
+    this.newChangeRequest.modiflyBy = this.infoAuth.fullname;
+    this.editChangeRequest.modiflyBy = this.infoAuth.fullname;
+    this.getChangeRequest();
+  },
+  watch: {
+    departmentIndex: function(index) {
+      this.department = this.departmentList[index].data.departmentName;
+      this.newChangeRequest.projectDepartment = this.departmentList[
+        index
+      ].data.departmentName;
+      this.getProject();
+    },
+    projectIndex: function(index) {
+      this.id = this.projectList[index].data.id;
+      this.newChangeRequest.project = this.projectList[index].data.projectId;
+      this.newChangeRequest.projectName = this.projectList[
+        index
+      ].data.projectName;
+      this.newChangeRequest.projectAmount = this.projectList[
+        index
+      ].data.projectAmount;
+    }
+  },
+  methods: {
+    async getChangeRequest() {
+      const searchData = [];
+      await jogetService
+        .list("thacExpense", "list_fin_advance", searchData)
+        .then(res => {
+          this.changeRequestList = res.data.data;
+        });
+    },
+    async getDepartment() {
+      const searchData = [];
+      await jogetService
+        .list("userAccountManagement", "listDepartment", searchData)
+        .then(res => {
+          let department = [];
+          department.push({
+            value: 0,
+            label: "กรุณาเลือกแผนก",
+            data: ""
+          });
+          res.data.data.forEach((item, index) => {
+            department.push({
+              value: index + 1,
+              label: item.departmentName,
+              data: item
+            });
+          });
+          this.departmentList = department;
+        });
+    },
+    async getProject() {
+      const searchData = [
+        {
+          paramName: "department",
+          paramValue: this.department
+        }
+      ];
+      await jogetService
+        .list("thacExpense", "list_project", searchData)
+        .then(res => {
+          let project = [];
+          project.push({
+            value: 0,
+            label: "กรุณาเลือกโครงการ",
+            data: ""
+          });
+          res.data.data.forEach((item, index) => {
+            project.push({
+              value: index + 1,
+              label: item.projectId + ": " + item.projectName,
+              data: item
+            });
+          });
+          this.projectList = project;
+        });
+    },
+
+    async getreasonList() {
+      await jogetService
+        .listAll("userAccountManagement", "listAllUsers")
+        .then(res => {
+          let requester = [];
+          res.data.data.forEach(item => {
+            requester.push({
+              value: item.fullname,
+              text: item.fullname
+            });
+          });
+          this.reasonList = requester;
+        });
+    },
+    async changeRequestCreate() {
+      await jogetService
+        .startProcess("thacExpense", "expenseClaim")
+        .then(res => {
+          this.newChangeRequest.processId = res.data.processId;
+          if (Number(this.newChangeRequest.value) <= 10000) {
+            let position = "ผู้อำนวยการฝ่าย" + this.department;
+            this.newChangeRequest.approval = position;
+          } else if (
+            Number(this.newChangeRequest.value) > 10000 &&
+            Number(this.newChangeRequest.value) <= 50000
+          ) {
+            this.newChangeRequest.approval =
+              "รองผู้อำนวยการสถาบันอนุญาโตตุลาการ";
+          } else if (Number(this.newChangeRequest.value) > 50000) {
+            this.newChangeRequest.approval = "ผู้อำนวยการสถาบันอนุญาโตตุลาการ";
+          }
+          this.newChangeRequest.processName = "รออนุมัติ";
+          jogetService
+            .formSubmit("thacExpense", "fin_advance", "", this.newChangeRequest)
+            .then(res => {
+              // this.calculateProject();
+              this.changeRequestCreateModal = false;
+              this.getChangeRequest();
+            });
+        });
+    },
+    async openchangeRequestEdit(item) {
+      this.changeRequestEditModal = true;
+      this.editChangeRequest.id = item.id;
+      this.editChangeRequest.subject = item.subject;
+      this.editChangeRequest.reason = item.reason;
+    },
+    async changeRequestEdit() {
+      await jogetService
+        .startProcess("thacExpense", "expenseClaim")
+        .then(res => {
+          jogetService
+            .formSubmit("thacExpense", "fin_advance", "", this.newChangeRequest)
+            .then(res => {
+              this.changeRequestCreateModal = false;
+              this.getChangeRequest();
+            });
+        });
+    },
+    considerRequestPage(changeRequestId) {
+      let routeData = this.$router.resolve({
+        name: "AddData",
+        query: { data: changeRequestId }
+      });
+      window.open(routeData.href, "_blank");
+    },
+    getBadge(status) {
+      switch (status) {
+        case "เสร็จสิ้น":
+          return "success";
+        case "รออนุมัติ":
+          return "info";
+        case "รอตรวจสอบ":
+          return "secondary";
+        case "รอผลดำเนินการ":
+          return "secondary";
+        default:
+          "primary";
+      }
+    }
+  }
+};
+</script>
