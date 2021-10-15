1d240d0ed389e474fc6a1e4d9c38a6bbfcf71509
60160113 <60160113@go.buu.ac.th>
2021-10-15 11:18:28 +0700

    menu

---
+++ src/containers/menu/1.data_center.js
@@ -0,0 +1,24 @@
+export default [
+    {
+      _name: "CSidebarNavTitle",
+      _children: ["Data Center"]
+    },
+    {
+      _name: "CSidebarNavItem",
+      name: "List All",
+      to: "/data-center/list-all",
+      icon: "cilLayers"
+    },
+    {
+      _name: "CSidebarNavItem",
+      name: "Check-in",
+      to: "/data-center/check-in",
+      icon: "cilLocationPin"
+    },
+    {
+      _name: "CSidebarNavItem",
+      name: "My Tasks",
+      to: "/data-center/view-tasks",
+      icon: "cilInbox"
+    }
+]
---
+++ src/containers/menu/4.change_request.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    _name: "CSidebarNavTitle",
+    _children: ["Change Request"]
+  },
+  {
+    _name: "CSidebarNavItem",
+    name: "Change Request",
+    to: "/change_request/changeRequest",
+    icon: "cil-circle"
+  }
+];
