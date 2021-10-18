1a0a33d25e1d04319d83405db88017066fdf15be
60160113 <60160113@go.buu.ac.th>
2021-10-18 19:03:09 +0700

    structure

--- src/router/information_security_incident.js
+++ src/router/information_security_incident.js
@@ -1,22 +1,40 @@
 export default [
-    {
-      path: "/information_security_incident",
-      name: "Information Security Incident",
-      component: () => import("@/views/information_security_incident/index.vue")
-    },
-    {
-      path: "/information_security_incident/inform",
-      name: "Information Security Incident Inform",
-      component: () => import("@/views/information_security_incident/AnalyzeIncident.vue")
-    },
-    {
-      path: "/information_security_incident/assign/:id/:process_id",
-      name: "Assign Operator",
-      component: () => import("@/views/information_security_incident/AssignOperator.vue")
-    },
-    {
-      path: "/information_security_incident/solving/:id/:process_id",
-      name: "Solve Problem",
-      component: () => import("@/views/information_security_incident/SolveProblem.vue")
-    },
-]
+  {
+    path: "/information_security_incident",
+    name: "Information Security Incident",
+    component: () => import("@/views/information_security_incident/index.vue")
+  },
+  {
+    path: "/information_security_incident/inform",
+    name: "Information Security Incident Inform",
+    component: () =>
+      import("@/views/information_security_incident/AnalyzeIncident.vue")
+  },
+  {
+    path: "/information_security_incident/assign/:id/:process_id",
+    name: "Assign Operator",
+    component: () =>
+      import("@/views/information_security_incident/AssignOperator.vue")
+  },
+  {
+    path: "/information_security_incident/solving/:id/:process_id",
+    name: "Solve Problem",
+    component: () =>
+      import("@/views/information_security_incident/SolveProblem.vue")
+  },
+  {
+    path: "/information_security_incident/inspect/:id/:process_id",
+    name: "Inspect",
+    component: () => import("@/views/information_security_incident/Inspect.vue")
+  },
+  {
+    path: "/information_security_incident/summary/:id/:process_id",
+    name: "Summary",
+    component: () => import("@/views/information_security_incident/Summary.vue")
+  },
+  {
+    path: "/information_security_incident/review/:id/:process_id",
+    name: "Review",
+    component: () => import("@/views/information_security_incident/Review.vue")
+  }
+];
---
+++ src/views/information_security_incident/Summary.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { authHeader } from "@/helpers/auth-header";
+import axios from "axios";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {
+    disabled_button() {
+      return (
+        Object.values(this.form).filter((item) => item === "").length > 0 ||
+        this.loading
+      );
+    },
+  },
+};
+</script>
