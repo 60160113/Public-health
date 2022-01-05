c128baa96f0375afd3f363b15f2852f6bc702a9b
60160113 <60160113@go.buu.ac.th>
2022-01-05 13:53:29 +0700

    comment

--- src/containers/menu/3.document_control.js
+++ src/containers/menu/3.document_control.js
@@ -1,30 +1,30 @@
 export default [
-    {
-      _name: "CSidebarNavTitle",
-      _children: ["Document Control"]
-    },
-    {
-      _name: "CSidebarNavItem",
-      name: "Add task",
-      to: "/document_control/add_document",
-      icon: "cil-circle"
-    },
-    {
-      _name: "CSidebarNavItem",
-      name: "My Tasks",
-      to: "/document_control",
-      icon: "cil-circle"
-    },
-    {
-      _name: "CSidebarNavItem",
-      name: "Files",
-      to: `/files/${process.env.VUE_APP_ALF_DOCUMENT_FOLDER_ID}`,
-      icon: "cil-circle"
-    },
-    {
-      _name: "CSidebarNavItem",
-      name: "Draft",
-      to: `/files/${process.env.VUE_APP_ALF_DRAFT_FOLDER_ID}`,
-      icon: "cil-circle"
-    },
-  ];
+  // {
+  //   _name: "CSidebarNavTitle",
+  //   _children: ["Document Control"]
+  // },
+  // {
+  //   _name: "CSidebarNavItem",
+  //   name: "Add task",
+  //   to: "/document_control/add_document",
+  //   icon: "cil-circle"
+  // },
+  // {
+  //   _name: "CSidebarNavItem",
+  //   name: "My Tasks",
+  //   to: "/document_control",
+  //   icon: "cil-circle"
+  // },
+  // {
+  //   _name: "CSidebarNavItem",
+  //   name: "Files",
+  //   to: `/files/${process.env.VUE_APP_ALF_DOCUMENT_FOLDER_ID}`,
+  //   icon: "cil-circle"
+  // },
+  // {
+  //   _name: "CSidebarNavItem",
+  //   name: "Draft",
+  //   to: `/files/${process.env.VUE_APP_ALF_DRAFT_FOLDER_ID}`,
+  //   icon: "cil-circle"
+  // },
+];
