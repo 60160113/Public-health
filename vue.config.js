d41cd55eebb3dff072e96e4828b3dd82dd8cbced
60160113 <60160113@go.buu.ac.th>
2022-01-11 17:31:03 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,6 @@ module.exports = {
   ],
   // use this option for production linking
   // publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/3.1.0' : '/'
-  publicPath: "/moph/"
+  publicPath: "/moph/",
+  outputDir: "moph"
 }
