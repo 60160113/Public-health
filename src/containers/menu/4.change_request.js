898bb7f32fd4d01a10d8dcdcd8df21b81404950d
60160113 <60160113@go.buu.ac.th>
2022-01-11 15:18:46 +0700

    Update 4.change_request.js

--- src/containers/menu/4.change_request.js
+++ src/containers/menu/4.change_request.js
@@ -1,12 +1,12 @@
 export default [
-  {
-    _name: "CSidebarNavTitle",
-    _children: ["Change Request"]
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Change Request",
-    to: "/change_request/changeRequest",
-    icon: "cil-circle"
-  }
+  // {
+  //   _name: "CSidebarNavTitle",
+  //   _children: ["Change Request"]
+  // },
+  // {
+  //   _name: "CSidebarNavItem",
+  //   name: "Change Request",
+  //   to: "/change_request/changeRequest",
+  //   icon: "cil-circle"
+  // }
 ];
