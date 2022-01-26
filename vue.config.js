f085e6335a303a60cf6736006d2b19fcca82a56c
60160113 <60160113@go.buu.ac.th>
2022-01-26 10:46:06 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,15 @@ module.exports = {
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
-       symlinks: false
+      symlinks: false
     }
   },
-  transpileDependencies: [
-    '@coreui/utils'
-  ],
+  transpileDependencies: ["@coreui/utils"],
   // use this option for production linking
   // publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/3.1.0' : '/'
   publicPath: "/moph/",
-  outputDir: "moph"
-}
+  outputDir: "moph",
+  chainWebpack: config => {
+    config.performance.maxEntrypointSize(400000).maxAssetSize(400000);
+  }
+};
