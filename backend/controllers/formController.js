dc07a5fd07256d0d2570ad25582549f40cec9c7f
60160113 <60160113@go.buu.ac.th>
2022-03-16 12:10:35 +0700

    Update formController.js

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -1,26 +1,51 @@
-const axios = require('axios')
-const jogetUser = require('../configs/json/JogetUser.json')
-const juser = jogetUser.jogetUser
+const axios = require("axios");
+const jogetUser = require("../configs/json/JogetUser.json");
+const juser = jogetUser.jogetUser;
 
 module.exports = {
   submit: async (req, res, next) => {
-    const reqApp = req.body.app
-    const primaryKey = req.body.primaryKey
-    const formData = req.body.formData
-    const permission = req.payload.permission
-    let formParam = ''
-    Object.keys(formData).forEach(function (key) {
-      formParam = `${formParam}&${key}=${formData[key]}`
-    })
-    const result = await axios.post(encodeURI(`${process.env.VUE_APP_JOGET_URL}web/json/data/form/store/${reqApp.appId}/${reqApp.formId}/${primaryKey}?j_username=${juser[permission].username}&j_password=${juser[permission].password}${formParam}`))
-    return result
+    const reqApp = req.body.app;
+    const primaryKey = req.body.primaryKey;
+    const formData = req.body.formData;
+    const permission = req.payload.permission;
+    let formParam = "";
+
+    if (Array.isArray(formData)) {
+      var resultArr = []
+      formData.forEach(async form => {
+        Object.keys(form).forEach(function(key) {
+          formParam = `${formParam}&${key}=${form[key]}`;
+        });
+        const result = await axios.post(
+          encodeURI(
+            `${process.env.VUE_APP_JOGET_URL}web/json/data/form/store/${reqApp.appId}/${reqApp.formId}/${primaryKey}?j_username=${juser[permission].username}&j_password=${juser[permission].password}${formParam}`
+          )
+        );
+        resultArr.push(result.data)
+      });
+      return resultArr
+    } else {
+      Object.keys(formData).forEach(function(key) {
+        formParam = `${formParam}&${key}=${formData[key]}`;
+      });
+      const result = await axios.post(
+        encodeURI(
+          `${process.env.VUE_APP_JOGET_URL}web/json/data/form/store/${reqApp.appId}/${reqApp.formId}/${primaryKey}?j_username=${juser[permission].username}&j_password=${juser[permission].password}${formParam}`
+        )
+      );
+      return result;
+    }
   },
 
   delete: async (req, res, next) => {
-    const reqApp = req.body.app
-    const primaryKey = req.body.primaryKey
-    const permission = req.payload.permission
-    const result = await axios.post(encodeURI(`${process.env.VUE_APP_JOGET_URL}web/json/data/form/delete/${reqApp.appId}/${reqApp.formId}/${primaryKey}?j_username=${juser[permission].username}&j_password=${juser[permission].password}`))
-    return result
+    const reqApp = req.body.app;
+    const primaryKey = req.body.primaryKey;
+    const permission = req.payload.permission;
+    const result = await axios.post(
+      encodeURI(
+        `${process.env.VUE_APP_JOGET_URL}web/json/data/form/delete/${reqApp.appId}/${reqApp.formId}/${primaryKey}?j_username=${juser[permission].username}&j_password=${juser[permission].password}`
+      )
+    );
+    return result;
   }
-}
+};
