4d16512322e63272d2f49aa68c53e4016c6ad571
60160113 <60160113@go.buu.ac.th>
2022-03-21 10:25:02 +0700

    update get backend

--- backend/controllers/datalistController.js
+++ backend/controllers/datalistController.js
@@ -1,39 +1,95 @@
-const axios = require('axios')
-const jogetUser = require('../configs/json/JogetUser.json')
-const juser = jogetUser.jogetUser
+const axios = require("axios");
+const jogetUser = require("../configs/json/JogetUser.json");
+const juser = jogetUser.jogetUser;
 
 module.exports = {
   getOne: async (req, res, next) => {
-    const permission = req.payload.permission
-    const reqApp = req.body.app
-    let params = ''
-    if (req.body.search) {
-      const search = req.body.search
-      search.forEach((param) => {
-        params = `${params}&${param.paramName}=${param.paramValue}`
-      })
+    try {
+      const permission = req.payload.permission;
+      const reqApp = req.body.app;
+      let params = "";
+      if (req.body.search) {
+        const search = req.body.search;
+        search.forEach(param => {
+          params = `${params}&${param.paramName}=${param.paramValue}`;
+        });
+      }
+      const result = await axios.post(
+        encodeURI(
+          `${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}${params}`
+        )
+      );
+      if (response.data.data.length > 1) {
+        res.send("Result more than one record");
+      } else if (result.data.data.length === 1) {
+        res.status(200).send(result.data);
+      } else {
+        res.send("No Data");
+      }
+    } catch (error) {
+      res.status(error.response.status);
     }
-    const result = await axios.post(encodeURI(`${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}${params}`))
-    return result
   },
   getAll: async (req, res, next) => {
-    const permission = req.payload.permission
-    const reqApp = req.body.app
-    
-    const result = await axios.post(encodeURI(`${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}`))
-    return result
+    const permission = req.payload.permission;
+    const reqApp = req.body.app;
+
+    try {
+      let start = 0;
+      let hasMoreItem = false;
+      const maxItem = 10;
+      let data = [];
+      do {
+        const url = encodeURI(
+          `${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}&start=${start}`
+        );
+        const responseData = await axios.post(url);
+        start += responseData.data.data.length;
+        data = [...data, ...responseData.data.data];
+
+        hasMoreItem =
+          responseData.data.data.length >= maxItem &&
+          responseData.data.total > start;
+      } while (hasMoreItem);
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send({ errorText: "Error on Get List" });
+    }
   },
   get: async (req, res, next) => {
-    const permission = req.payload.permission
-    const reqApp = req.body.app
-    let params = ''
-    if (req.body.search) {
-      const search = req.body.search
-      search.forEach((param) => {
-        params = `${params}&${param.paramName}=${param.paramValue}`
-      })
+    const permission = req.payload.permission;
+    const reqApp = req.body.app;
+    let params = "";
+    try {
+      if (req.body.search) {
+        const search = req.body.search;
+        search.forEach(param => {
+          params = `${params}&${param.paramName}=${param.paramValue}`;
+        });
+      }
+      let start = 0;
+      let hasMoreItem = false;
+      const maxItem = 10;
+      let data = [];
+      do {
+        const url = encodeURI(
+          `${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}${params}&start=${start}`
+        );
+        const responseData = await axios.post(url);
+        start += responseData.data.data.length;
+        data = [...data, ...responseData.data.data];
+        if (
+          responseData.data.data.length >= maxItem &&
+          responseData.data.total > start
+        ) {
+          hasMoreItem = true;
+        } else {
+          hasMoreItem = false;
+        }
+      } while (hasMoreItem);
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send({ errorText: "Error on Get List" });
     }
-    const result = await axios.post(encodeURI(`${process.env.APP_JOGET_URL}web/json/data/list/${reqApp.appId}/${reqApp.listId}/?j_username=${juser[permission].username}&j_password=${juser[permission].password}${params}`))
-    return result
   }
-}
+};
--- backend/routes/datalistRoute.js
+++ backend/routes/datalistRoute.js
@@ -3,38 +3,10 @@ const { required } = require('../configs/auth')
 const auth = require('../configs/auth')
 const datalistController = require('../controllers/datalistController')
 
-router.post('/getOne', auth.required, (req, res, next) => {
-  datalistController.getOne(req)
-    .then((response) => {
-      if (response.data.data.length > 1) {
-        res.send('Result more than one record')
-      } else if (response.data.data.length === 1) {
-        res.status(200).send(response.data)
-      } else {
-        res.send('No Data')
-      }
-    }).catch((error) => {
-      res.status(error.status)
-    })
-})
+router.post('/getOne', auth.required, datalistController.getOne)
 
-router.post('/getAll', auth.required, (req, res, next) => {
-  datalistController.getAll(req)
-    .then((response) => {
-      res.status(200).send(response.data)
-    }).catch((error) => {
-      res.status(error.status)
-    })
-})
+router.post('/getAll', auth.required, datalistController.getAll)
 
-router.post('/get', auth.required, (req, res, next) => {
-  datalistController.get(req)
-    .then((response) => {
-      res.status(200).send(response.data)
-    }).catch((error) => {
-      console.log(error)
-      res.status(error.status)
-    })
-})
+router.post('/get', auth.required, datalistController.get)
 
 module.exports = router
