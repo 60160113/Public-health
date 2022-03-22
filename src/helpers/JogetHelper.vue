d899644f16d419fff416d4ba83806beec5d4a604
60160113 <60160113@go.buu.ac.th>
2022-03-22 12:38:03 +0700

    Create JogetHelper.vue

---
+++ src/helpers/JogetHelper.vue
@@ -0,0 +1,144 @@
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    // return header
+    axiosOptions() {
+      const AuthUser = JSON.parse(localStorage.getItem("AuthUser"));
+      // return authorization header with jwt token
+      if (AuthUser && AuthUser.token) {
+        return {
+          headers: { Authorization: "Bearer " + AuthUser.token },
+        };
+      } else {
+        return { headers: {} };
+      }
+    },
+
+    // ==== PROCESS ====//
+    // start process
+    async startProcess(appId, processDefId) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          processDefId: processDefId,
+        },
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/process/start`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+    // complete process
+    async processComplete(activityId, variables = null) {
+      var axiosData = {
+        activityId: activityId,
+      };
+      if (variables) {
+        axiosData["variables"] = variables;
+      }
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/process/complete${
+          variables ? "WithVariable" : ""
+        }`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+
+    // ==== ACTIVITY ==== //
+    // current activity
+    async getCurrentActivity(processId) {
+      const axiosData = {
+        processId: processId,
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/process/view`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+
+    // ==== FORM ====//
+    // submit
+    async formSubmit(appId, formId, primaryKey, formData) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          formId: formId,
+        },
+        primaryKey: primaryKey,
+        formData: formData,
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/form/submit`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+    // delete
+    async formDelete(appId, formId, primaryKey) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          formId: formId,
+        },
+        primaryKey: primaryKey,
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/form/delete`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+
+    // ==== LIST ====//
+    // get
+    async list(appId, listId, searchData) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          listId: listId,
+        },
+        search: searchData,
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/list/get`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+    // get all
+    async listAll(appId, listId) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          listId: listId,
+        },
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/list/getAll`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+    // get one
+    async getOne(appId, listId, searchData) {
+      const axiosData = {
+        app: {
+          appId: appId,
+          listId: listId,
+        },
+        search: searchData,
+      };
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/list/getOne`,
+        axiosData,
+        this.axiosOptions()
+      );
+    },
+  },
+};
+</script>
