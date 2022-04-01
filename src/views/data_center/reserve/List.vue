ddfad23631a566454fa234141b4c57532f414e7c
60160113 <60160113@go.buu.ac.th>
2022-04-01 15:40:55 +0700

    Update List.vue

--- src/views/data_center/reserve/List.vue
+++ src/views/data_center/reserve/List.vue
@@ -18,11 +18,57 @@
           :itemsPerPageSelect="{
             label: 'แสดง',
           }"
+          column-filter
+          hover
           :loading="loading"
         >
           <template #no-items-view
             ><div class="text-center">ไม่พบข้อมูล</div>
           </template>
+
+          <template #hasHardwareStatus="{ item }"
+            ><td>
+              <CBadge
+                :color="
+                  JSON.parse(item.display_hasHardwares) ? 'success' : 'danger'
+                "
+                style="font-size: 16px; width: 100%"
+                class="mt-1"
+                ><b>{{ item.hasHardwareStatus }}</b></CBadge
+              >
+            </td>
+          </template>
+
+          <template #actions="{ index }">
+            <td>
+              <CButton
+                color="primary"
+                variant="outline"
+                square
+                size="sm"
+                block
+                @click="toggleIndex = toggleIndex == index ? -1 : index"
+              >
+                แสดง
+              </CButton>
+            </td>
+          </template>
+
+          <template #details="{ index }">
+            <CCollapse :show="toggleIndex == index" :duration="150">
+              <div class="mt-2 mb-2">
+                <CButton color="success" class="ml-2"><b>อนุมัติ</b> </CButton>
+                <CButton color="danger" class="ml-2"
+                  ><b>ไม่อนุมัติ</b>
+                </CButton>
+                <CButton color="info" class="ml-2"
+                  ><b>ดูรายละเอียด</b>
+                </CButton>
+              </div>
+            </CCollapse>
+          </template>
+
+          <template #actions-filter> <p></p> </template>
         </CDataTable>
       </CCardBody>
     </CCard>
@@ -41,23 +87,53 @@ export default {
       list: [],
 
       fields: [
-        { key: "request_id", label: "ID", _style: "width:10%" },
-        { key: "reserve_date", label: "วันที่ร้องขอ", _style: "width:20%" },
-
+        { key: "request_id", label: "ID", _style: "width:20%" },
+        { key: "reserve_date", label: "วันที่ร้องขอ", _style: "width:15%" },
+        {
+          key: "display_requesterName",
+          label: "ผู้ร้องขอ",
+          _style: "width:15%",
+        },
+        {
+          key: "display_requesterPosition",
+          label: "ตำแหน่ง",
+          _style: "width:10%",
+        },
+        {
+          key: "display_requesterAffiliation",
+          label: "สังกัด/บริษัท",
+          _style: "width:10%",
+        },
         { key: "objective", label: "วัตถุประสงค์", _style: "width:15%" },
-        { key: "action", label: "ดำเนินการ", _style: "width:10%" },
+        {
+          key: "hasHardwareStatus",
+          label: "มีการนำเข้า - ออก Hardwares",
+          _style: "width:5%",
+        },
+        { key: "actions", label: "ดำเนินการ", _style: "width:10%" },
       ],
 
       loading: false,
+      toggleIndex: -1,
     };
   },
   methods: {
+    // get list
     getList() {
       this.loading = true;
 
       this.jogetListAll("mophApp", "list_data_center_reserve")
         .then((res) => {
-          this.list = res.data;
+          this.list = res.data.map((item) => {
+            item.hasHardwareStatus = JSON.parse(item.display_hasHardwares)
+              ? "มี"
+              : "ไม่มี";
+            item.reserve_date =
+              new Date(item.reserve_date).toLocaleDateString() +
+              " " +
+              new Date(item.reserve_date).toLocaleTimeString();
+            return item;
+          });
           this.loading = false;
         })
         .catch((err) => {
