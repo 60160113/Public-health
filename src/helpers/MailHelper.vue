28e41f5616f53059f22547e4e888a157a3bb3e50
60160113 <60160113@go.buu.ac.th>
2022-04-20 14:49:27 +0700

    Create MailHelper.vue

---
+++ src/helpers/MailHelper.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    // return header
+    mailAxiosOptions(localStorageIndex = "") {
+      const storageIndex = localStorageIndex ? localStorageIndex : "AuthUser";
+      const AuthUser = JSON.parse(localStorage.getItem(storageIndex));
+      // return authorization header with jwt token
+      if (AuthUser && AuthUser.token) {
+        return {
+          headers: { Authorization: "Bearer " + AuthUser.token },
+        };
+      } else {
+        return { headers: {} };
+      }
+    },
+    sendMail(to, subject, html, from = "", localStorageIndex = "AuthUser") {
+      var axiosData = {
+        to,
+        subject,
+        html,
+      };
+      if (from) {
+        axiosData["from"] = from;
+      }
+      return await axios.post(
+        `${process.env.VUE_APP_BACKEND_URL}/mail/send${
+          Array.isArray(to) ? "/multiple" : ""
+        }`,
+        axiosData,
+        this.mailAxiosOptions(localStorageIndex)
+      );
+    },
+  },
+};
+</script>
