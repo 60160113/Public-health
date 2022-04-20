1dc34ca79e85acb00e60129481ba387bb574c0b6
60160113 <60160113@go.buu.ac.th>
2022-04-20 14:34:13 +0700

    mail

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -15,13 +15,39 @@ const transporter = nodemailer.createTransport({
 
 module.exports = {
   send: async (req, res, next) => {
-    const mailOptions = req.body;
+    const mailOptions = { ...req.body };
+    if (!mailOptions.from) {
+      mailOptions.from = process.env.APP_MAIL_USER;
+    }
     await transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
-        res.send(err);
+        res.status(err.responseCode).send(err);
       } else {
         res.send(info);
       }
     });
+  },
+  sendMultiple: async (req, res, next) => {
+    try {
+      var response = [];
+
+      var mailOptions = { ...req.body };
+      if (!mailOptions.from) {
+        mailOptions.from = process.env.APP_MAIL_USER;
+      }
+      for await (const elem of req.body.to) {
+        mailOptions.to = elem;
+        await transporter.sendMail(mailOptions, async function(err, info) {
+          if (err) {
+            throw err;
+          } else {
+            await response.push(info);
+          }
+        });
+      }
+      await res.send(response);
+    } catch (error) {
+      res.send(error);
+    }
   }
 };
--- backend/routes/mailRoute.js
+++ backend/routes/mailRoute.js
@@ -1,8 +1,10 @@
-const router = require('express').Router()
-const { required } = require('../configs/auth')
-const auth = require('../configs/auth')
-const mailController = require('../controllers/mailController')
+const router = require("express").Router();
+const { required } = require("../configs/auth");
+const auth = require("../configs/auth");
+const mailController = require("../controllers/mailController");
 
-router.post('/send', auth.required, mailController.send)
+router.post("/send", auth.required, mailController.send);
 
-module.exports = router
+router.post("/send/multiple", auth.required, mailController.sendMultiple);
+
+module.exports = router;
