53ddaef7621e718da41b682cfe8cb24758e988d9
60160113 <60160113@go.buu.ac.th>
2022-04-22 11:26:56 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,128 +1,49 @@
 <template>
   <div>
-    <!-- Data Center -->
     <CCard>
-      <CCardHeader
-        ><strong class="text-primary">Data center</strong></CCardHeader
-      >
       <CCardBody>
+        <!-- Content -->
         <CRow>
-          <CCol md="4">
-            <CCard>
-              <CCardHeader color="warning">
-                <h4 style="color: white">จอง</h4></CCardHeader
-              >
-              <CCardBody
-                ><CButton
-                  @click="openPage('/data-center/reserve', true)"
-                  color="primary"
-                  >เปิดในแท็บใหม่</CButton
-                ></CCardBody
-              >
-            </CCard>
-          </CCol>
-          <CCol md="4"
-            ><CCard>
-              <CCardHeader color="primary">
-                <h4 style="color: white">เช็คอิน</h4></CCardHeader
-              >
-              <CCardBody
-                ><CButton
-                  @click="openPage('/data-center/check-in')"
-                  color="primary"
-                  >เปิด</CButton
-                >&nbsp;<CButton
-                  @click="openPage('/data-center/check-in', true)"
-                  color="primary"
-                  >เปิดในแท็บใหม่</CButton
-                ></CCardBody
-              >
-            </CCard></CCol
-          >
-          <CCol md="4"
-            ><CCard>
-              <CCardHeader color="success">
-                <h4 style="color: white">งานของฉัน</h4></CCardHeader
-              >
-              <CCardBody
-                ><CButton
-                  @click="openPage('/data-center/view-tasks')"
-                  color="primary"
-                  >เปิด</CButton
-                >&nbsp;<CButton
-                  @click="openPage('/data-center/view-tasks', true)"
-                  color="primary"
-                  >เปิดในแท็บใหม่</CButton
-                ></CCardBody
-              >
-            </CCard></CCol
-          >
-          <CCol md="4">
-            <CCard>
-              <CCardHeader color="info">
-                <h4 style="color: white">รายการทั้งหมด</h4></CCardHeader
-              >
-              <CCardBody
-                ><CButton
-                  @click="openPage('/data-center/list-all')"
-                  color="primary"
-                  >เปิด</CButton
-                >&nbsp;<CButton
-                  @click="openPage('/data-center/list-all', true)"
-                  color="primary"
-                  >เปิดในแท็บใหม่</CButton
-                ></CCardBody
-              >
-            </CCard>
+          <CCol md="3" :key="index" v-for="(item, index) in dataCenterCards">
+            <CCardGroup>
+              <CCard>
+                <CCardHeader color="info"
+                  ><h4>{{ item.label }}</h4></CCardHeader
+                >
+                <CCardBody>
+                  <CCardText
+                    ><h5>{{ item.value }}</h5></CCardText
+                  >
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
           </CCol>
         </CRow>
+        <!-- Content -->
       </CCardBody>
-    </CCard>
-
-    <!-- Configuration -->
-    <CCard>
-      <CCardHeader
-        ><strong class="text-primary">Configuration</strong></CCardHeader
-      >
-      <CCardBody>
-        <CRow>
-          <CCol md="4"
-            ><CCard>
-              <CCardHeader color="primary">
-                <h4 style="color: white">วัตถุประสงค์</h4></CCardHeader
-              >
-              <CCardBody
-                ><CButton
-                  @click="openPage('/configuration/data_center/objectives')"
-                  color="primary"
-                  >เปิด</CButton
-                >&nbsp;<CButton
-                  @click="
-                    openPage('/configuration/data_center/objectives', true)
-                  "
-                  color="primary"
-                  >เปิดในแท็บใหม่</CButton
-                ></CCardBody
-              >
-            </CCard></CCol
-          >
-        </CRow>
-      </CCardBody>
+      <CElementCover :opacity="0.8" v-if="loading">
+        <h1 class="d-inline">Loading...</h1>
+        <CSpinner size="5xl" color="success" />
+      </CElementCover>
     </CCard>
   </div>
 </template>
 
 <script>
+import JogetHelper from "@/helpers/JogetHelper";
 export default {
+  mixins: [JogetHelper],
+  data() {
+    return {
+      loading: false,
+
+      dataCenterCards: [],
+    };
+  },
   methods: {
-    openPage(to, newTab = false) {
-      if (newTab) {
-        let routeData = this.$router.resolve(to);
-        window.open(routeData.href, "_blank");
-      } else {
-        this.$router.push(to);
-      }
-    },
+    async getDataCenterCards() {
+      
+    }
   },
 };
 </script>
