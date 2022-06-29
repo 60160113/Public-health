623b044f0ac6a92b4f1c71d5a460e6f391d1f44e
60160113 <60160113@go.buu.ac.th>
2022-06-29 11:39:35 +0700

    Update 4.change_request.js

--- src/containers/menu/4.change_request.js
+++ src/containers/menu/4.change_request.js
@@ -1,12 +1,17 @@
-export default [
-  // {
-  //   _name: "CSidebarNavTitle",
-  //   _children: ["Change Request"]
-  // },
-  // {
-  //   _name: "CSidebarNavItem",
-  //   name: "Change Request",
-  //   to: "/change_request/changeRequest",
-  //   icon: "cil-circle"
-  // }
+export default [{
+        _name: "CSidebarNavTitle",
+        _children: ["Change Request"]
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "ร้องขอดำเนินการเปลี่ยนแปลง",
+        to: "/change_request/request",
+        icon: "cil-circle"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "ถาดงานดำเนินการเปลี่ยนแปลง",
+        to: "/change_request/List",
+        icon: "cilLayers"
+    }
 ];
